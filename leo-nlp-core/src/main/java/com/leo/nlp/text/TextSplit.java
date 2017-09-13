f82e2c3da54ead89c49426796607d5982a8cf304
jiangfeng07 <1154142410@qq.com>
2017-09-13 18:32:37 +0800

    modify

---
+++ leo-nlp-core/src/main/java/com/leo/nlp/text/TextSplit.java
@@ -0,0 +1,22 @@
+package com.leo.nlp.text;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by lionel on 17/9/13.
+ */
+public class TextSplit {
+    private static final String SENTENCE_DELIMITERS = "[?!;？！.。；\\r\\n]";
+
+    public static String[] sentenceSplit(String sentence) {
+        if (StringUtils.isBlank(sentence)) {
+            return null;
+        }
+
+        String[] res = sentence.split(SENTENCE_DELIMITERS);
+        if (res.length > 0) {
+            return res;
+        }
+        return null;
+    }
+}
